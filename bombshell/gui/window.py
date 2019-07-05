64537f188d2182edd24775692bc37743d85a4ade
9uapaw <gyorandr@gmail.com>
2019-07-05 14:20:03 +0200

    Tweak GUI

--- bombshell/gui/window.py
+++ bombshell/gui/window.py
@@ -8,18 +8,19 @@ import PySimpleGUIQt as sg
 from core.game_loop import GameLoop
 
 sg.SetOptions(button_color=sg.COLOR_SYSTEM_DEFAULT)
-sg.ChangeLookAndFeel('GreenTan')
+# sg.ChangeLookAndFeel('GreenTan')
 
+BUTTON_SIZE = (80, 20)
 
 def run_window():
-    debug_line = sg.Output()
+    debug_line = sg.Output(size=(400, 300))
     layout = [
         [sg.Text('Waypoint path: '), sg.InputText(key='waypoint'),
-         sg.Button('Record'), sg.Button('Stop record')],
-        [sg.Text('Open waypoint'), sg.InputText(key='waypoint'), sg.FileBrowse(key='Browse')],
+         sg.Button('Record', size=BUTTON_SIZE), sg.Button('Stop record', size=BUTTON_SIZE)],
+        [sg.Text('Open waypoint'), sg.InputText(key='waypoint'), sg.FileBrowse(key='Browse', size=BUTTON_SIZE)],
         [debug_line],
-        [sg.Button('Start bot'), sg.Cancel('Stop bot')],
-        [sg.CloseButton("Close")]
+        [sg.Button('Start bot', size=BUTTON_SIZE), sg.Cancel('Stop bot', size=BUTTON_SIZE)],
+        [sg.CloseButton("Close", size=BUTTON_SIZE)]
 
     ]
 
@@ -37,7 +38,6 @@ def run_window():
         elif event == 'Start bot':
             paths['waypoint'] = values['waypoint']
             t.start()
-            # game_loop.start(paths)
         elif event == 'Stop record' or event == 'Stop bot':
             game_loop.screen.stop_capturing()
             t.join()
