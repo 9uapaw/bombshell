ba3a50c22450e227e896aa9156ee0278edb9db87
godrisprox <andristarr@gmail.com>
2019-09-27 11:17:00 +0200

    + Added ability to find loot icon on screen.

--- bombshell/image/screenscuttler.py
+++ bombshell/image/screenscuttler.py
@@ -11,6 +11,36 @@ class ScreenScuttler:
     width_scale = 9.0112359
     height_scale = 52.6666667
 
+    def try_find_in_screen(self, template: PilImage, screenshot: PilImage, threshold: int = 50) -> Tuple[
+        Tuple[int, int], Tuple[int, int], Tuple[int, int], Tuple[int, int]]:
+        template_cvf = cv2.cvtColor(numpy.array(template), cv2.COLOR_RGB2GRAY)
+        screenshot_cvf = cv2.cvtColor(numpy.array(screenshot), cv2.COLOR_RGB2GRAY)
+
+        w, h = template_cvf.shape[::-1]
+
+        meth = ['cv2.TM_CCOEFF_NORMED']
+
+        method = eval(meth[0])
+        res = cv2.matchTemplate(screenshot_cvf, template_cvf, method)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+        if (max_val*100) <= threshold:
+            return None
+
+        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+            top_left = min_loc
+        else:
+            top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+
+        cv2.rectangle(screenshot_cvf, top_left, bottom_right, 255, 2)
+
+        ret = ((top_left[0], top_left[1]), (bottom_right[0], top_left[1]), (bottom_right[0], bottom_right[1]),(top_left[0], bottom_right[1]))
+
+        print("DEBUG: Found coordinates: {}".format(ret))
+
+        return ret
+
     def find_in_screen(self, template: PilImage, screenshot: PilImage) -> Tuple[
         Tuple[int, int], Tuple[int, int], Tuple[int, int], Tuple[int, int]]:
         template_cvf = cv2.cvtColor(numpy.array(template), cv2.COLOR_RGB2GRAY)
@@ -58,3 +88,9 @@ class ScreenScuttler:
         accept_btn_image = PilImage.open(os.path.join(currdir, '../assets/image/release_button.png'))
         return self.find_in_screen(accept_btn_image, screenshot)
 
+    def find_loot_icon(self, screenshot: PilImage) -> Tuple[
+        Tuple[int, int], Tuple[int, int], Tuple[int, int], Tuple[int, int]]:
+
+        currdir = os.path.dirname(__file__)
+        loot_icon_image = PilImage.open(os.path.join(currdir, '../assets/image/loot_icon.png'))
+        return self.try_find_in_screen(loot_icon_image, screenshot, 70)
