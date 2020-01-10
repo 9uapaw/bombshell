491d24e6ce426a45c487111d91e8c9be97fcadaf
9uapaw <gyorandr@gmail.com>
2020-01-10 16:04:55 +0100

    Replace text with pixels in addon - initial version

--- addon/BagBuddy.lua
+++
@@ -1,3 +0,0 @@
-function BagBuddy_OnLoad(self)
-   SetPortraitToTexture(self.portrait, "Interface\\Icons\\INV_Misc_EngGizmos_30")
-end
---
+++ addon/core/px.lua
@@ -0,0 +1,85 @@
+local A, T = ...
+
+T.ToNormalizedRGB = ToNormalizedRGB
+T.ToHex = ToHex
+T.ToRGB = ToRGB
+T.split = split
+T.replace_char = replace_char
+T.starts_with = starts_with
+
+local b = "10101010111000110"
+
+function ToHex(number, base)
+  if number == "-1" then
+    return "FFFFFF"
+  end
+
+  local hex = number 
+  if (base ~= 16) then
+    hex = tonumber(number, base)
+  else
+    return number
+  end
+
+  return string.format("%06x", hex)
+end
+
+function ToRGB(hex)
+  local rgb = {r=0, g=0, b=0}
+
+  rgb["r"] = tonumber(hex:sub(1, 2), 16)
+  rgb["g"] = tonumber(hex:sub(3, 4), 16)
+  rgb["b"] = tonumber(hex:sub(5, 6), 16)
+
+  return rgb
+end
+
+function ToNormalizedRGB(rgb)
+  rgb["r"] = rgb["r"] / 255
+  rgb["g"] = rgb["g"] / 255
+  rgb["b"] = rgb["b"] / 255
+
+  return rgb
+end
+
+function FloatToNormalizedRGB(float)
+  local intPart, decPart = string.match(tostring(float), "([^.]*)%.([^.]*)")
+
+  local decPart = string.sub(decPart, 0, 6)
+  local decHex = ToHex(decPart, 10)
+  local fullHex = intPart..decHex
+
+  return ToNormalizedRGB(ToRGB(fullHex))
+end
+
+function FloatToNormalizedRGBPairs(float)
+  local intPart, decPart = string.match(tostring(float), "([^.]*)%.([^.]*)")
+
+  local intHex = ToHex(intPart, 10)
+  local decHex = ToHex(string.sub(decPart, 0, 6), 10)
+  
+  local rgbs = {}
+  rgbs[1] = ToNormalizedRGB(ToRGB(intHex))
+  rgbs[2] = ToNormalizedRGB(ToRGB(decHex))
+
+  return rgbs
+end
+
+function starts_with(str, start)
+   return str:sub(1, #start) == start
+end
+
+function split(s, delimiter)
+    result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
+
+function replace_char(pos, str, r)
+    return str:sub(1, pos-1) .. r .. str:sub(pos+1)
+end
+
+print(FloatToNormalizedRGBPairs("343.2315123235")[2]["b"])
+print(ToHex("010201", 16))
